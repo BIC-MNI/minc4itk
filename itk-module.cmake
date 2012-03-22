91e25fa8b7031cb769306d00c67372ee139f66e3
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-03-22 11:18:10 -0400

    initial configuration

---
+++ itk-module.cmake
@@ -0,0 +1,14 @@
+set(DOCUMENTATION "This module Implements minc IO file format ...")
+ 
+itk_module(
+    ITKIOMINC
+  DEPENDS
+    ITKHDF5
+    ITKZLIB
+    ITKCommon
+    ITKIOImageBase
+  TEST_DEPENDS
+    ITKTestKernel
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
